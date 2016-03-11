c3a0ca3d38ed9ca7bcb096d636578ccacd4a56f4
Craig Trudo <craig.trudo@walmart.com>
2016-03-10 23:20:11 -0600

    looping exercise

---
+++ ListBuilder.js
@@ -0,0 +1,32 @@
+/**
+ * Created by ctrudo on 3/10/16.
+ */
+window.onload = function(){
+    var tweets = getTweets();
+};
+
+function buildList(response){
+    var tweets = response.tweets;
+    var listElement = document.getElementById('twitter-list');
+
+    for(var i = 0; i < tweets.length; i++){
+        var div = document.createElement("div");
+        var node = document.createTextNode(tweets[i].text);
+        div.appendChild(node);
+        listElement.appendChild(div);
+    }
+}
+
+function getTweets(){
+    var xmlhttp = new XMLHttpRequest();
+    var url = "tweets.json";
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var response = JSON.parse(xmlhttp.responseText);
+            buildList(response);
+        }
+    };
+    xmlhttp.open("GET", url);
+    xmlhttp.send();
+}
